3f5b4f102297b69f88be513ff995ef5ee058c407
Siddharth <112213674+SiddharthN21@users.noreply.github.com>
2023-12-29 18:11:58 -0500

    Delete X86 Assembly Language, C, and C++ Projects/Project 1 directory

--- X86 Assembly Language, C, and C++ Projects/Project 1/mysort.h
+++
@@ -1,7 +0,0 @@
-
-// generic comparison function
-typedef int (*CompareFunction) (void *a, void *b);
-
-// Prototype for mysort()
-void mysort( int n, int elementSize, void * array,
-             int ascending, CompareFunction compFunc );
