44ce0b6159a307c4b85aa3124b10aa27e4cca6fa
Siddharth <112213674+SiddharthN21@users.noreply.github.com>
2023-12-29 18:10:33 -0500

    Added

---
+++ X86 Assembly Language, C, and C++ Projects/Project 1/mysort.h
@@ -0,0 +1,7 @@
+
+// generic comparison function
+typedef int (*CompareFunction) (void *a, void *b);
+
+// Prototype for mysort()
+void mysort( int n, int elementSize, void * array,
+             int ascending, CompareFunction compFunc );
